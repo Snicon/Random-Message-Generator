5eae84ff9b0d068de585e3ee64e3821c335f0c2b
Sixten Peterson <sixten.peterson@winterzone.se>
2022-12-28 18:36:24 +0100

    Added an array of quotes.

---
+++ main.js
@@ -0,0 +1,11 @@
+/*  Messages are stored as strings in an array. 
+    Could have made something fun with objects that have categories etc but I'm keeping this project minimal for now.
+    No point in making a ton of features for something I won't use. */
+
+const quotes = [
+    '"If it can be null, it will be null."\n -Unknown',
+    '"Truth can only be found in one place: the code."\n -Robert C. Martin',
+    '"Ruby is rubbish! PHP is phpantastic!"\n -Nikita Popov',
+    '"Java is to JavaScript what car is to carpet."\n -Chris Heilmann',
+    '"It works on my machine"\n -Unknown'
+]
