f7d31fff0ca7f0cf814e4f4c43995bcccad6851f
Sixten Peterson <sixten.peterson@winterzone.se>
2022-12-28 18:47:38 +0100

    Implemented a function for getting the quotes.

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ const quotes = [
     '"Java is to JavaScript what car is to carpet."\n -Chris Heilmann',
     '"It works on my machine"\n -Unknown'
 ]
+
+// ES 6 arrow function for getting quotes. Not really any point to make this a function but hey, I made one anyways.
+// The array.length is obviously used to in order to allow for easier addition of quotes... Feel free to make a pull request with more quotes just because.
+const getRandomQuote = array => {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+// Call the function with the quotes array as a selection. Is it elegant? No. Does it work? Yes. Will I ever touch this codebase again? Hopefully not. 
+console.log(getRandomQuote(quotes));
+
+// It's been nice having you around, have a great day. 💅
